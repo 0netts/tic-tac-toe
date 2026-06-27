6e32c61c8905296296f8f2ad385f87fccfd169a2
AshDev <99580600+0netts@users.noreply.github.com>
2026-06-27 11:48:52 +0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,68 +1,59 @@
-#include <iostream>
-#include <string>
+include<iostream>
 
-int  main() {
-  system("clear");
-  int x,y;
-   char board[3][3] {
-      {'.', '.', '.'},
-      {'.', '.', '.'},
-      {'.', '.', '.'},
+
+bool isVictory = false; 
+
+bool isWon(char board[3][3], char res) {
+   for (int i  = 0; i < 3;i++) {
+       if (board[0][i] == res && board[1][i] == res && board[2][i] == res) { return true;}
+   }
+   for (int x = 0; x < 3;x++) {
+       if (board[x][0] == res && board[x][1] == res && board[x][2] == res) {return true;}
+   }
+   
+   if (board[0][0] == res && board[1][1] == res && board[2][2] == res) {
+        return true;
+   }
+   if (board[0][2] == res && board[1][1] == res && board[2][0] == res) { return true;}
+   
+    return false;
+}
+
+void DrawMenu(char board[3][3], int posY, int posX, char res) {
+     if (posY < 1 || posY > 3 || posX < 1 || posX > 3) {std::cout << "  You have crossed the border" << std::endl; return;}
+     if (board[posY - 1][posX - 1] != '.') { std::cout << "Busy!!" << std::endl; return ; }
+     
+     board[posY- 1][posX - 1] = res;
+     if (isWon(board, res)) {
+       std::cout << "player " << res << " is won" << std::endl;
+       isVictory = true;
+     }
+     for (int xc = 0; xc < 3;xc++) {
+        for (int xy = 0; xy < 3; xy++) {
+            std::cout << " " << board[xc][xy] ;
+        }
+        std::cout << std::endl;
+     }
+     
+}
+
+
+int main()
+{
+    char matrix[3][3] = {
+       {'.','.','.'},
+       {'.','.','.'},
+       {'.','.','.'},
+    
     };
-  bool PlayerVictory = false;
-
-  while (!PlayerVictory) {
-      for (int y = 0; y < 3; y++) {
-          for (int x = 0; x < 3; x++) {
-              std::cout << board[y][x] << " ";
-          }
-          std::cout << std::endl;
-      }
-
-
-      std::cout << "Player 1 please Enter your possition: ";
+    int x,y;
+    while (!isVictory) {
+      std::cout << "player X: ";
+      std::cin  >> x >> y;
+      DrawMenu(matrix,y,x,'x');
+      std::cout << "Player O: ";
       std::cin >> x >> y;
-      if (board[y-1][x-1] != '.') {
-          std::cout << "busy!! \n";
-          continue;
-      }
-
-      board[y-1][x-1] = 'x';
-      if ((board[0][0] == 'x' && board[0][1] == 'x' && board[0][2] == 'x') ||
-          (board[1][0] == 'x' && board[1][1] == 'x' && board[1][2] == 'x') ||
-          (board[2][0] == 'x' && board[2][1] == 'x' && board[2][2] == 'x') ||
-          (board[0][0] == 'x' && board[1][0] == 'x' && board[2][0] == 'x') ||
-          (board[0][1] == 'x' && board[1][1] == 'x' && board[2][1] == 'x') ||
-          (board[0][2] == 'x' && board[1][2] == 'x' && board[2][2] == 'x') ||
-          (board[0][0] == 'x' && board[1][1] == 'x' && board[2][2] == 'x') ||
-          (board[0][2] == 'x' && board[1][1] == 'x' && board[2][0] == 'x')) {
-              PlayerVictory = true;
-              std::cout << "Player 1 is win!!\n";
-              break;
-          }
-
-     std::cout << "PLayer 2 please ENter your possition: ";
-      std::cin >> x >> y;
-      if (board[y-1][x-1] != '.') {
-
-          std::cout << "busy!! \n";
-          continue;
-      }
-      board[y-1][x-1] = 'o';
-      if ((board[0][0] == 'o' && board[0][1] == 'o' && board[0][2] == 'o') ||
-          (board[1][0] == 'o' && board[1][1] == 'o' && board[1][2] == 'o') ||
-          (board[2][0] == 'o' && board[2][1] == 'o' && board[2][2] == 'o') ||
-          (board[0][0] == 'o' && board[1][0] == 'o' && board[2][0] == 'o') ||
-          (board[0][1] == 'o' && board[1][1] == 'o' && board[2][1] == 'o') ||
-          (board[0][2] == 'o' && board[1][2] == 'o' && board[2][2] == 'o') ||
-          (board[0][0] == 'o' && board[1][1] == 'o' && board[2][2] == 'o') ||
-          (board[0][2] == 'o' && board[1][1] == 'o' && board[2][0] == 'o')) {
-              PlayerVictory = true;
-              std::cout << "Player 2 is win!!\n";
-              break;
-          }
-
-  }
-
- return 0;
+      DrawMenu(matrix,y,x,'o');
+    }
+    return 0;
 }
